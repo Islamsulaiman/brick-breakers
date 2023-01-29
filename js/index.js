5aaa55b844a71322063bcbd6740f547cdf4199e5
islam mustafa sulaiman <ma.eslam@yahoo.com>
2023-01-29 20:56:36 +0200

    adding sounds

--- js/index.js
+++ js/index.js
@@ -14,3 +14,14 @@ CRACKED_IMG.src = "images/crackedPNG.PNG";
 const boardWidth = 90;
 const boardHeight = 15;
 const boardMarginBottom = 30;
+
+const sounds = {
+  ballHitBrick: new Audio("/sounds/brick.mp3"),
+  ballHitBoard: new Audio("/sounds/brick.mp3"),
+  gameStart: new Audio("/sounds/brick.mp3"),
+  gameFinish: new Audio("/sounds/game-over.mp3"),
+  nextLevel: new Audio("/sounds/level-up.mp3"),
+  brickCrack: new Audio("/sounds/brick_hit.mp3"), //Sound Effect from pixapay
+  pauseGameSound: new Audio("/sounds/pause.mp3"), //Sound Effect from pixapay
+  onLoadSound: new Audio("/sounds/game_start.mp3"), //Sound Effect from pixapay
+};
